b9d749529d62cdbed934005ccf317efaa8b0f5d6
Jack Stanbridge <supbscripter@gmail.com>
2020-02-09 01:36:16 +0000

    Added rolling left

--- Godot/Scripts/Ship.cs
+++ Godot/Scripts/Ship.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Ship : Node2D
 {
@@ -8,6 +9,12 @@ public class Ship : Node2D
 	// private string b = "text";
 
 	private int speed = 10;
+
+	private bool readyToRoll = true;
+	private List<bool> rollInputQueue = new List<bool>();
+	private int rollInputDuration = 100;
+	private int rollInputCountdown;
+	private bool rollInputStarted = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -18,23 +25,61 @@ public class Ship : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		Vector2 input = new Vector2();
+		// Movement
+		Vector2 move_input = new Vector2();
 		if (Input.IsActionPressed("move_forward")) 
-		{
-			input.y--;
-		}
+			move_input.y-= Input.GetActionStrength("move_forward"); ;
 		if (Input.IsActionPressed("move_backward"))
-		{
-			input.y++;
-		}
+			move_input.y+= Input.GetActionStrength("move_backward");
 		if (Input.IsActionPressed("move_left"))
-		{
-			input.x--;
-		}
+			move_input.x-= Input.GetActionStrength("move_left");
 		if (Input.IsActionPressed("move_right"))
+			move_input.x+= Input.GetActionStrength("move_right");
+
+		if (move_input.Length() > 1)
+			move_input = move_input.Normalized();
+		Position += move_input * speed;
+
+		// Rolling
+		if (Input.IsActionJustPressed("roll_left")) 
 		{
-			input.x++;
+			rollInputQueue.Add(true);
+			if (rollInputStarted == false)
+			{
+				rollInputStarted = true;
+				rollInputCountdown = rollInputDuration;
+			}
 		}
-		Position += input.Normalized() * speed;
+		else if (Input.IsActionJustReleased("roll_left") && rollInputStarted)
+		{
+			rollInputQueue.Add(false);
+		}
+
+		if (rollInputQueue.Count == 3 && rollInputQueue[0] == true && rollInputQueue[1] == false && rollInputQueue[2] == true)
+		{
+			Position += new Vector2(-100.0f, 0.0f);
+			rollInputStarted = false;
+			rollInputCountdown = rollInputDuration;
+			rollInputQueue.Clear();
+		}
+
+		if (rollInputStarted)
+		{
+			rollInputCountdown--;
+			if (rollInputCountdown <= 0)
+			{
+				rollInputStarted = false;
+				rollInputCountdown = rollInputDuration;
+				rollInputQueue.Clear();
+			}
+		}
+
+		if (rollInputQueue.Count > 3)
+		{
+			rollInputQueue.RemoveRange(0, rollInputQueue.Count - 3);
+		}
+
+		//var result = "blah " + string.Join(", ", rollInputQueue.ToArray());
+		//GD.Print(result);
 	}
 }
